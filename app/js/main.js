bc1ee1ee8af5a73a596bed442b0ab9a54786e5ad
catteri <caterinascob@gmail.com>
2021-01-18 09:44:57 -0500

    added top slider section

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.top__slider-inner').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpead:2000
+    });
+
+});
